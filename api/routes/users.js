ae5198e0102a92848370e605e771b8feb50bd633
reserviert97 <nurlatif97@outlook.com>
2019-07-09 20:54:26 +0700

    add user login with jwt

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
   User.find()
@@ -48,30 +50,88 @@ router.post('/register', (req, res) => {
     user_photo
   } = req.body;
 
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    user_name,
-    user_email,
-    user_password,
-    user_address,
-    user_role,
-    user_rek_number,
-    user_postalcode,
-    user_photo
-  });
 
-  user
-    .save()
-    .then(result => {
-      console.log(result)
-      res.status(201).json({
-        status: 201,
-        message: 'new user successfully created',
-        user: result
-      })
+  User.find({user_email: user_email})
+    .then(user => {
+      if (user >= 1) {
+        return res.status(409).json({message: "Mail Exist"});
+      } else {
+        bcrypt.hash(req.body.user_password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({error: err});
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              user_password: hash,
+              user_name,
+              user_email,
+              user_address,
+              user_role,
+              user_rek_number,
+              user_postalcode,
+              user_photo
+            });
+            user.save()
+              .then(result => {
+                res.status(201).json({
+                  message: 'User Created',
+                  createdUser: result
+                })
+              })
+              .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                  error: err
+                })
+              });
+          }
+        
+        });
+      }
     })
     .catch(err => {
       console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    });
+
+  
+});
+
+router.post('/login', (req, res) => {
+  User.find({user_email: req.body.user_email})
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({message: 'Auth Failed'});
+      }
+      bcrypt.compare(req.body.user_password, user[0].user_password, (err, result) => {
+        if (err) {
+          return res.status(401).json({message: 'Auth Failed'});
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].user_email,
+              userId: user[0]._id
+            }, 
+            process.env.JWT_KEY || 'secret',
+            {
+              expiresIn: "12h"
+            }
+          );
+          return res.status(200).json({
+            message: 'Auth Success',
+            token: token
+          });
+        }
+
+        return res.status(401).json({message: 'Auth Failed'});
+      })
+    })
+    .catch(err => {
+      console.log(err);
       res.status(500).json({
         error: err
       })
