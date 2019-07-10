30aebf9a2935e3dc23e412a3b366702984326fc1
eko <febri1412@gmail.com>
2019-07-10 11:20:14 +0700

    add get cart by Id

--- api/models/product.js
+++ api/models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
   product_price: {
     type: Number,
     required: true
--- api/models/tmpCart.js
+++ api/models/tmpCart.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
-
 const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  tmp_no_order: { 
-    type: String,
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId,
     required: true 
   },
- tmp_session_order: {
-    type: String,
-    required: true
-  },
-  tmp_number_of_product: {
+  products: [{ 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true 
+  }],
+  total_of_product: {
     type: Number,
     required: true
   },
-  tmp_amount_ordered: {
+  total_amount_ordered: {
     type: Number, 
     required: true
   },
